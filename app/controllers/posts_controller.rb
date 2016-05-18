4cfeeebbfe148fc0d15a5675bda52a7df751cab4
mfq <softmfq@gmail.com>
2016-05-18 18:32:53 +0500

    added action for create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,21 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.new(post_params)
+		if @post.save
+			render :json => { status: true, post: @post }
+		else
+			render :json => { status: false }
+		end
 	end
 
 	def show
+	end
+
+	private 
+
+	def post_params
+		params.require(:posts).permit(:title, :body)
 	end
 
 end
